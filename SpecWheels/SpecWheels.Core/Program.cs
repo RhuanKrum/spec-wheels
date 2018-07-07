d632f06ebe542df4734d63cf7d092196246da5f0
Rhuan Krum <rhuan.krum@hotmail.com>
2018-07-06 22:05:41 -0400

    Update project structure to separate it in Core, Web and Tests for best practices

---
+++ SpecWheels/SpecWheels.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpecWheels.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
