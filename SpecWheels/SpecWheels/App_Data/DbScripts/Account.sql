bc167b2a435aec449e3bb75c5b9443f692a0bef8
Rhuan Krum <rhuan.krum@hotmail.com>
2018-08-07 07:52:23 -0400

    Developed user use cases and defined project structure
    - Developed Register User
    - Developed Login/Logout
    - Developed View user data
    - Removed Test and Web projects, so we just stick with the default "SpecWhells" project.
    - The SpecWheels project was changed to use the Asp.Net Framework 7.1
    - Created the folder DbScripts to store the default create table and inserts for the DB
    - Updated the Layout view
    - Defined the temporary field validations (to be improved later)

---
+++ SpecWheels/SpecWheels/App_Data/DbScripts/Account.sql
@@ -0,0 +1,23 @@
+﻿-- Execute after DB is created 
+INSERT INTO [dbo].[AspNetRoles]
+           ([Id]
+           ,[Name])
+     VALUES
+           (0, 'Administrator');
+GO
+
+INSERT INTO [dbo].[AspNetRoles]
+           ([Id]
+           ,[Name])
+     VALUES
+           (1, 'Moderator');
+GO
+
+INSERT INTO [dbo].[AspNetRoles]
+           ([Id]
+           ,[Name])
+     VALUES
+           (2, 'User');
+GO
+
+
