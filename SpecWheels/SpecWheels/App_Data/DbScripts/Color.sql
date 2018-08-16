88c8582221c18258168e11732681bf670f318873
DaianaArantes <32435352+DaianaArantes@users.noreply.github.com>
2018-08-16 07:48:38 -0400

    add db files

---
+++ SpecWheels/SpecWheels/App_Data/DbScripts/Color.sql
@@ -0,0 +1,4 @@
+CREATE TABLE [dbo].[Color](
+	[Id] [int]PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](128) NOT NULL
+	)
---
+++ SpecWheels/SpecWheels/App_Data/DbScripts/Suspension.sql
@@ -0,0 +1,4 @@
+CREATE TABLE [dbo].[Suspension](
+	[Id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[Brand] [nvarchar](40) NOT NULL,
+	[Name] [nvarchar](40) NOT NULL)
