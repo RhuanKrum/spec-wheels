7a985d32d6c10510c84abe90d5197de38b867f23
Rhuan Krum <rhuan.krum@hotmail.com>
2018-08-18 21:21:42 -0400

    Create template class to deal with DB interactions

---
+++ SpecWheels/SpecWheels/Models/TEMP_DELETE/TemplateModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpecWheels.Models.TEMP_DELETE
+{
+    public class TemplateModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
