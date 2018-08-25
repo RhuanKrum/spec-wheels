6af76b4e1520cb6cbd9f4af031091bf603c128b1
Rhuan Krum <rhuan.krum@hotmail.com>
2018-08-25 14:36:09 -0400

    Created test project and template class for Break test

---
+++ SpecWheels/SpecWheels.Test/BreakTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpecWheels.Models.Break;
+
+namespace SpecWheels.Test
+{
+    [TestClass]
+    public class BreakTest
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            BreakModel breakModel = new BreakModel();
+            breakModel.Brand = "Brembo";
+            breakModel.Name = "Brembo";
+            breakModel.Size = "400 mm";
+            breakModel.Type = "Racing";
+
+            Assert.IsNotNull(breakModel);
+            //BreakDataAccess breakDataAccess = new BreakDataAccess();
+            //breakDataAccess.Create(breakModel);
+
+            //Assert.ThrowsException(breakDataAccess.Create(breakModel));
+        }
+    }
+}
