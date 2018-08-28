ed0a4d0ad4eb7cf3f2288f998c2965d12272dd25
DaianaArantes <32435352+DaianaArantes@users.noreply.github.com>
2018-08-28 14:14:53 -0400

    partial commit. Failing to attach db

---
+++ SpecWheels/SpecWheels/Controllers/BreakController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using SpecWheels.Models.Break;
+using SpecWheels.Models.User;
+
+namespace SpecWheels.Controllers
+{
+    [Authorize]
+    public class BreakController : Controller
+    {
+        public BreakController()
+        {
+        }
+        
+        
+        //
+        // GET: /Account/Register
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(BreakModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                BreakDataAccess breakDataAccess = new BreakDataAccess();
+                breakDataAccess.Create(model);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+
+        
+
+        //
+        // GET: /Account/View
+        //[Authorize]
+        //public new ActionResult View()
+        //{
+        //    UserModel account = UserManager.FindById(User.Identity.GetUserId());
+        //    return View(account);
+        //}
+        //
+        // GET: /Account/Edit
+        [AllowAnonymous]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Edit
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Edit(UserModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var currentUser = UserManager.FindByEmail(model.Email);
+
+        //        currentUser.FirstName = model.FirstName;
+        //        currentUser.LastName = model.LastName;
+        //        currentUser.Nickname = model.Nickname;
+        //        currentUser.Password = model.Password;
+
+        //        var result = await UserManager.UpdateAsync(currentUser);
+        //        if (result.Succeeded)
+        //        {
+        //            return RedirectToAction("View", "User");
+        //        }
+        //        else
+        //        {
+        //            AddErrors(result);
+        //        }
+        //    }
+
+        //    // If we got this far, something failed, redisplay form
+        //    return View(model);
+        //}
+
+      
+        //
+        //// POST: /Account/Delete
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Inactivate(UserModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var currentUser = UserManager.FindByEmail(model.Email);
+
+        //        currentUser.FirstName = model.FirstName;
+        //        currentUser.LastName = model.LastName;
+        //        currentUser.Nickname = model.Nickname;
+        //        currentUser.Password = model.Password;
+        //        currentUser.InactiveDate = DateTime.Now;
+
+        //        var result = await UserManager.UpdateAsync(currentUser);
+        //        if (result.Succeeded)
+        //        {
+        //            // If the inactivated account belongs to the user, log him out
+        //            if (User.Identity.GetUserId().Equals(currentUser.Id))
+        //            {
+        //                Logout();
+        //            }
+        //            return RedirectToAction("View", "User");
+        //        }
+        //        else
+        //        {
+        //            AddErrors(result);
+        //        }
+        //    }
+        //    // If we got this far, something failed, redisplay form
+        //    return View(model);
+        //}
+
+
+
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        
+        public enum ManageMessageId
+        {
+            ChangePasswordSuccess,
+            SetPasswordSuccess,
+            RemoveLoginSuccess,
+            Error
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        private ActionResult RedirectToLocal()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        
+    }
+}
---
+++ SpecWheels/SpecWheels/Models/Break/BreakDataAccess.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+
+using System.Web;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SpecWheels.Models.Break
+{
+    public class BreakDataAccess
+    {
+        public string appPath;
+        public string connectionString;
+
+        //public string connectionString = @".\SQLEXPRESS; Initial Catalog=SpecWheels; Integrated Security=true";
+
+        public BreakDataAccess()
+        {
+            appPath = @"C:\Users\daian\source\repos\spec-wheels\SpecWheels\SpecWheels\App_Data";
+            connectionString = @"Data Source=DAYAPC\SQLEXPRESS;AttachDbFilename=" + @appPath + @"\SpecWheels.mdf;Initial Catalog=SpecWheels;Integrated Security=True; User Instance=TRUE";
+
+        }
+
+        public bool Create (BreakModel model)
+        {
+            String sql = "insert into Break (brand, Name, Size, Type) values (@Brand, @Name, @Size, @Type)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@Brand", model.Brand);
+                command.Parameters.AddWithValue("@Name", model.Name);
+                command.Parameters.AddWithValue("@Size", model.Size);
+                command.Parameters.AddWithValue("@Type", model.Type);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+                return true;
+            }
+        }
+
+        public BreakModel Read (int id)
+        {
+            var model = new BreakModel();
+
+            String sql = "select brand, Name, Size, Type from Break where Id=@Id";
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = command.ExecuteReader();
+                model.Id = id;
+                model.Name = reader["Name"].ToString();
+                model.Brand = reader["brand"].ToString();
+                model.Size = reader["Size"].ToString();
+                model.Type = reader["Type"].ToString();
+            }
+
+            return model;
+        }
+
+        public void Delete (BreakModel model)
+        {
+            String sql = "delete from Break where Id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+               
+                command.Parameters.AddWithValue("@Id", model.Id);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Update (BreakModel model)
+        {
+            String sql = "Update Break set Brand=@Brand, Name=@Name, Size=@Size, Type=@Type where Id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+
+                command.Parameters.AddWithValue("@Brand", model.Brand);
+                command.Parameters.AddWithValue("@Name", model.Name);
+                command.Parameters.AddWithValue("@Size", model.Size);
+                command.Parameters.AddWithValue("@Type", model.Type);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
