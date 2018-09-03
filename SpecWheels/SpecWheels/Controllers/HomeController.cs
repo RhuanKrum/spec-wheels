da44812c3ed3833be76b1101fcb763ea78c84091
Rhuan Krum <rhuan.krum@hotmail.com>
2018-09-03 18:44:39 -0400

    Fix Issue 12 - No default home page (#16)

---
+++ SpecWheels/SpecWheels/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using SpecWheels.Models.Break;
+using SpecWheels.Models.User;
+
+namespace SpecWheels.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+    
+}
